85ebb1e7878b5733e030148e45af9995c189410d
Anton Belmisov <antonbelmisov@mail.ru>
2022-08-30 10:09:23 +0300

    Выполнено задание 1

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,10 +1,25 @@
+//Задание 1
+
 function compareArrays(arr1, arr2) {
-  let result;
+  let result = true;
 
   // Ваш код
+  if (arr1.lenght != arr2.lenght) {
+    return false; // Массивы разной длины не равны
+  }
+  // Цикл по всем элементам
+  for (let i = 0; i < arr1.length; i++) {
+    // Если хоть один элемент отличается, массивы не равны
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    } 
+    
+  }
 
   return result; // boolean
 }
+
+//Задание 2
 
 function advancedFilter(arr) {
   let resultArr;
