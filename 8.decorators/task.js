a2ce62650c4fd708bca4bdc4f2bbfc98cf201724
Anton Belmisov <antonbelmisov@mail.ru>
2022-09-18 16:58:43 +0300

    выполнено задание 1

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,5 +1,23 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = args.join(',');
+    let objectInCache = cache.find((item) => item.hash === hash);
+    if (objectInCache) {
+      console.log("Из кэша: " + objectInCache.value);
+      return "Из кэша: " + objectInCache.value;
+    }
+
+    let result = func(...args);
+    cache.push({hash: hash, value: result});
+    if (cache.lenght > 5) {
+      cache.shift();
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+  return wrapper;
 }
 
 
